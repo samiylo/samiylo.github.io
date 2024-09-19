4ddcfdca7c7d431a0487132b7eb2caf790dbc6d7
samiylo <skryshu@gmail.com>
2024-09-18 22:46:53 -0500

    Add Navbar: social icons svg not working not sure why

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
 // import { Banner } from "./components/Banner";
 // import { Skills } from "./components/Skills";
 // import { Projects } from "./components/Projects";
@@ -10,8 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar />
-      <Banner />
+      <NavBar />
+      {/* <Banner />
       <Skills />
       <Projects />
       <Contact />
