f231d8db39eec3e5978214bb9007865fc16ebd4c
samiylo <skryshu@gmail.com>
2024-09-19 12:26:25 -0500

    Fix footer icons & disclaimer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-// import logo from "../assets/img/logo.svg";
-// import navIcon1 from "../assets/img/nav-icon1.svg";
-// import navIcon2 from "../assets/img/nav-icon2.svg";
-// import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from "../assets/img/logo.png";
+import { SocialIcon } from "react-social-icons";
+import 'react-social-icons/discord';
+import 'react-social-icons/twitter';
+import 'react-social-icons/github';
 
 export const Footer = () => {
   return (
@@ -12,15 +13,15 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            {/* <img src={logo} alt="Logo" /> */}
+            <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              {/* <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
+            <SocialIcon bgColor="white" fgColor="purple" url="https://discord.com"/>
+                <SocialIcon bgColor="white" fgColor="purple" url="https://x.com"/>
+                <SocialIcon bgColor="white" fgColor="purple" url="https://github.com"/>
             </div>
-            <p>Copyright 2004. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
