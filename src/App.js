60c46ccaea06c37fc8bd58fef9ad1d333b15b18f
samiylo <skryshu@gmail.com>
2024-09-18 22:32:16 -0500

    Nuke and config for react 18

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-// import Menu from "./pages/Menu";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import ProductDetail from "./pages/ProductDetail";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { NavBar } from "./components/NavBar";
+// import { Banner } from "./components/Banner";
+// import { Skills } from "./components/Skills";
+// import { Projects } from "./components/Projects";
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-         
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/product/:name" component={ProductDetail} />
-        </Switch>
-        {/* <Footer /> */}
-      </Router>
+      {/* <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer /> */}
     </div>
   );
 }
