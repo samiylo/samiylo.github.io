44bdcca20ddca86547ba5e16b3e2ebd152812d68
samiylo <skryshu@gmail.com>
2024-09-18 23:35:15 -0500

    Pupulate lorum ipsum text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,7 +51,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Explore my key projects that showcase a blend of innovative problem-solving, robust design, and real-world application. From backend services to full-stack solutions, each project reflects my dedication to clean code, scalability, and delivering high-quality software.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
