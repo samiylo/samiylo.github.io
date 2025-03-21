706bba2f32e7cc73230029c0f831f2ccf3c83e1a
samiylo <skryshu@gmail.com>
2025-03-20 21:21:17 -0500

    external: lime initial setup

--- src/components/external/lglj/LgljService.js
+++ src/components/external/lglj/LgljService.js
@@ -1,6 +1,7 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../../../assets/img/project-img1.png"
+import { Color } from 'three';
 
 export const Lglj = () => {
   const responsive = {
@@ -30,19 +31,26 @@ export const Lglj = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>LGLJ Dashboard</h2>
+                        
+                        <h5>Derek McCallum</h5>
+                        <a className='web-links' href="https://www.lg-lj.net/">lglj.net</a>
+                        <p>Bakery & Accessories </p>
+                        
+                        <h1>Analytics Recommendations NPS</h1>
+                        <p>Improved Analytics</p>
+                        <p>Real Time Processing</p>
+                        <p>90% Reduction in calls</p>
+
+                        <h1>Logo Design #1</h1>
                         <p>Dashboard for Dericks LGLJ Company</p>
 
-                        <h2>LGLJ Dashboard</h2>
+                        <h1>Logo Design #2</h1>
                         <p>Dashboard for Dericks LGLJ Company</p>
 
-                        <h2>LGLJ Dashboard</h2>
+                        <h1>Logo Design #3</h1>
                         <p>Dashboard for Dericks LGLJ Company</p>
 
-                        <h2>LGLJ Dashboard</h2>
-                        <p>Dashboard for Dericks LGLJ Company</p>
-
-                        <h2>LGLJ Dashboard</h2>
-                        <p>Dashboard for Dericks LGLJ Company</p>
+                        
                         
                     </div>
                 </div>
