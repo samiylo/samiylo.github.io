ba13bf3bfbfd4626321e914ce252bedfa4a4ed4a
samiylo <skryshu@gmail.com>
2025-06-17 22:12:10 -0500

    security: lint project

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}; 
