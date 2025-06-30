7adbe86e6f3aa4872b8503aed791f0451be05682
samiylo <skryshu@gmail.com>
2025-06-29 21:16:48 -0500

    remove dead code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Timeline } from "./components/Timeline";
 import { Contact } from "./components/Contact";
@@ -21,10 +20,8 @@ function App() {
                 <>
                 <NavBar />
                 <Banner />
-
-                <Projects />
                 <Timeline />
-                <Skills />
+                <Projects />
                 <Contact />
                 <Footer />
                 </>
