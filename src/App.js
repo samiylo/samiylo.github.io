5601b0f94d52adca7d7438b27cdd5601404d3f63
samiylo <skryshu@gmail.com>
2025-07-30 10:23:23 -0500

    Remove unused dependency

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Achievements } from "./components/Achievements";
 import { Timeline } from "./components/Timeline";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
